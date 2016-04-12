2b1238fe551d2b6c21bb2a53fc6ee700e0539ebb
zohan180 <hzhuo2@wisc.edu>
2016-04-12 18:50:30 -0500

    Update channel_trigger_logic.sv

--- channel_trigger_logic.sv
+++ channel_trigger_logic.sv
@@ -1,41 +1,48 @@
 module channel_trigger_logic(clk, set_armed, CHxHff5, CHxLff5, CHxTrigCfg, ChxTrig);
 
-input logic clk, set_armed, CHxHff5, CHxLff5;
-input logic [4:0] CHxTrigCfg; //we only use the 5 bits of the register
+input clk, set_armed, CHxHff5, CHxLff5;
+input [4:0] CHxTrigCfg; //we only use the 5 bits of the register
 
 output logic ChxTrig;
 
 logic low_level, high_level, negative_edge, positive_edge;
-logic ll_ff, hl_ff, ne_ff, pe_ff; //high level flip flop etc.
-logic ne, pe; //negative edge, positive edge;
+logic Hff1_1, Hff1_2, Lff1_1, Lff1_2, Hff2, Lff2; //ff ni between, from top to bottom
 
+assign low_level = Lff2 & CHxTrigCfg[1];
+assign high_level = Hff2 & CHxTrigCfg[2];
+assign negative_edge = Lff1_2 & CHxTrigCfg[3];
+assign positive_edge = Hff1_2 & CHxTrigCfg[4];
+assign ChxTrig = CHxTrigCfg[0] | low_level | high_level | negative_edge | positive_edge;
 
-assign low_level = ll_ff & CHxTrigCfg[1];
-assign high_level = hl_ff & CHxTrigCfg[2];
-assign negative_edge = ne_ff & CHxTrigCfg[3];
-assign positive_edge = pe_ff & CHxTrigCfg[4];
-assign ChxTrig = (CHxTrigCfg[0] | low_level | high_level | negative_edge | positive_edge);
-
-//clock flops
-always_ff @(posedge clk) begin
-	ll_ff <= CHxLff5;
-	hl_ff <= CHxHff5;
-	ne_ff <= ne;
-	pe_ff <= pe;
-end
-
-//negative edge flop
-always_ff @(negedge CHxLff5)
+//1'b1 CHxHff5 flop
+always_ff @(posedge CHxHff5, set_armed) 
 	if(!set_armed)
-		ne <= 1'b1;
-	else
-		ne <= 1'b0;
+		Hff1_1 <= 1'b0;
+	else 
+		Hff1_1 <= 1'b1;
 
-//positive edge flop
-always_ff @(posedge CHxHff5)
+always_ff @ (posedge clk) 
+	Hff1_2 <= Hff1_1;
+
+
+//1'b1 CHxLff5 flop
+always_ff @(negedge CHxLff5, set_armed)
 	if(!set_armed)
-		pe <= 1'b1;
+		Lff1_1 <= 1'b1;
 	else
-		pe <= 1'b0;
+		Lff1_1 <= 1'b0;
+
+always_ff @ (posedge clk)
+	Lff1_2 <= Lff1_1;
+
+
+//CHxHff5 flop
+always_ff @(posedge clk)
+	Hff2 <= CHxHff5;
+
+//CHxLff5 flop
+always_ff @(posedge clk)
+	Lff2 <= CHxLff5;
+
 		
-endmodule		
+endmodule
