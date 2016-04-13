ce60bf10f8c74dec7166105de922034a19d36588
8bitMoose <mmuccianti@gmail.com>
2016-04-13 18:46:12 -0500

    Major changes
    There were many things wrong with the old version.
    This is my version from HW2

--- pwm8.sv
+++ pwm8.sv
@@ -1,33 +1,25 @@
-module pwm8(PWM_sig, clk, rst_n, duty);
+module pwm8(clk, rst_n, duty, PWM_sig);
 
-input [7:0] duty;
 input clk, rst_n;
+input [7:0] duty;
 output reg PWM_sig;
 
-logic [7:0] cnt;
-logic set, reset;
+reg [7:0] counter;
 
-// 8 bit counter
-always @(posedge clk or negedge rst_n) begin
-	assign cnt = (!rst_n) ? 8'h00: cnt + 1; 
+always_ff @(posedge clk, negedge rst_n)
+	if(!rst_n)
+		counter <= 0;
+	else
+		counter <= counter + 1;
+
+always_ff @(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		PWM_sig <= 0;
+	else if(&counter)
+		PWM_sig <= 1;
+	else if(counter == duty)
+		PWM_sig <= 0;
 end
 
-// generate set and reset signal for flip flop
-always @(cnt) begin
- 	assign reset = (cnt==duty)? 1:0;
-	assign set = (&cnt==0'b1)? 1:0;
-end
-
-//flip flop that generates PWM_sig 
-always @(posedge clk or negedge rst_n) begin
-	if (!rst_n)
-	PWM_sig <= 1'b0;
-	else if (reset)
-	PWM_sig <= 1'b0;
-	else if (set)
-	PWM_sig <= 1'b1;
-	else 
-	PWM_sig <= PWM_sig;
-end
 
 endmodule
