55bf64cca494259543e3954c45f01901b2d2c596
8bitMoose <mmuccianti@gmail.com>
2016-04-13 19:06:48 -0500

    reset logic was wrong
    we always reset to 0.
    then on the correct edge of the signal, the input will go to 1 and the trigger will occur

--- channel_trigger_logic.sv
+++ channel_trigger_logic.sv
@@ -6,7 +6,7 @@ input [4:0] CHxTrigCfg; //we only use the 5 bits of the register
 output logic ChxTrig;
 
 logic low_level, high_level, negative_edge, positive_edge;
-logic Hff1_1, Hff1_2, Lff1_1, Lff1_2, Hff2, Lff2; //ff ni between, from top to bottom
+logic Hff1_1, Hff1_2, Lff1_1, Lff1_2, Hff2, Lff2; //ff in between, from top to bottom
 
 assign low_level = Lff2 & CHxTrigCfg[1];
 assign high_level = Hff2 & CHxTrigCfg[2];
@@ -15,34 +15,37 @@ assign positive_edge = Hff1_2 & CHxTrigCfg[4];
 assign ChxTrig = CHxTrigCfg[0] | low_level | high_level | negative_edge | positive_edge;
 
 //1'b1 CHxHff5 flop
-always_ff @(posedge CHxHff5, set_armed) 
+always_ff @(posedge CHxHff5, negedge set_armed) begin
 	if(!set_armed)
-		Hff1_1 <= 1'b0;
+		Hff1_1 <= 1'b0; //TODO: how do we know what to reset to?
 	else 
 		Hff1_1 <= 1'b1;
+end
 
-always_ff @ (posedge clk) 
+always_ff @(posedge clk) begin
 	Hff1_2 <= Hff1_1;
-
+end
 
 //1'b1 CHxLff5 flop
-always_ff @(negedge CHxLff5, set_armed)
+always_ff @(negedge CHxLff5, negedge set_armed) begin
 	if(!set_armed)
-		Lff1_1 <= 1'b1;
+		Lff1_1 <= 1'b0; //TODO: how do we know what to reset to?
 	else
-		Lff1_1 <= 1'b0;
+		Lff1_1 <= 1'b1;
+end
 
-always_ff @ (posedge clk)
+always_ff @(posedge clk) begin
 	Lff1_2 <= Lff1_1;
-
+end
 
 //CHxHff5 flop
-always_ff @(posedge clk)
+always_ff @(posedge clk) begin
 	Hff2 <= CHxHff5;
+end
 
 //CHxLff5 flop
-always_ff @(posedge clk)
+always_ff @(posedge clk) begin
 	Lff2 <= CHxLff5;
-
+end
 		
 endmodule
