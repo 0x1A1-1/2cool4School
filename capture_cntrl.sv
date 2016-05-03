88be965345f28c97747d816af9bab3859f4cf471
zohan180 <hzhuo2@wisc.edu>
2016-05-03 01:24:27 -0500

    Update capture_cntrl.sv

--- capture_cntrl.sv
+++ capture_cntrl.sv
@@ -23,26 +23,46 @@ module capture_cntrl(clk, rst_n, wrt_smpl, we, waddr, trig_posH, trig_posL, run,
 	logic set_armed, clr_armed;
 	logic waddr_inc, waddr_rst;
 	
-	assign armed = clr_armed ? 1'b0 : (set_armed ? 1'b1 : armed); 
-
 	assign trig_pos = {trig_posH, trig_posL};
+		/****8
+	assign trig_cnt = trig_cnt_rst ? 0 : (trig_cnt_inc ? trig_cnt + 1 : trig_cnt);
+
+	assign smpl_cnt = smpl_cnt_rst ? 0 : (smpl_cnt_inc ? smpl_cnt + 1 : smpl_cnt);
+
+	assign waddr = waddr_rst ? 0 : (waddr_inc ? waddr + 1 : waddr);
+	****/
+	always_ff @ (posedge clk, negedge rst_n) begin 
+ 		if(!rst_n) 
+ 			armed <= 1'b0; 
+ 		else if (set_armed)
+ 			armed <= 1'b1;
+		else if(clr_armed)
+			armed <= 1'b0;
+ 	end 	
 	
-	always_ff @ (posedge clk) begin 
- 		if(waddr_rst) 
+	
+	always_ff @ (posedge clk, negedge rst_n) begin 
+ 		if(!rst_n) 
+ 			waddr <= 0; 
+		else if(waddr_rst) 
  			waddr <= 0; 
  		else if (waddr_inc)
  			waddr <= waddr + 1; 
  	end 
 	
-	always_ff @ (posedge clk) begin 
- 		if(smpl_cnt_rst) 
+	always_ff @ (posedge clk, negedge rst_n) begin 
+	 	if(!rst_n) 
+ 			smpl_cnt <= 0; 
+ 		else if(smpl_cnt_rst) 
  			smpl_cnt <= 0; 
  		else if (smpl_cnt_inc)
  			smpl_cnt <= smpl_cnt + 1; 
  	end 
 
-	always_ff @ (posedge clk) begin 
- 		if(trig_cnt_rst) 
+	always_ff @ (posedge clk, negedge rst_n) begin
+	 	if(!rst_n) 
+ 			trig_cnt <= 0; 
+ 		else if(trig_cnt_rst) 
  			trig_cnt <= 0; 
  		else if (trig_cnt_inc)
  			trig_cnt <= trig_cnt + 1; 
@@ -50,7 +70,7 @@ module capture_cntrl(clk, rst_n, wrt_smpl, we, waddr, trig_posH, trig_posL, run,
 	
 	
 	//main FSM		 
- 	typedef enum reg [2:0] {IDLE, RUN, TRIG, SMPL, DONE} state_t; 
+ 	typedef enum reg [2:0] {SMPL, DONE, TRIG, IDLE, RUN} state_t; 
  	state_t state, nxt_state; 
  	 
  	always_ff @ (posedge clk, negedge rst_n) begin 
@@ -74,50 +94,59 @@ module capture_cntrl(clk, rst_n, wrt_smpl, we, waddr, trig_posH, trig_posL, run,
 		waddr_inc = 0;
 
 		case(state)
-			IDLE: 	if (run) begin
+			IDLE: 	begin
+					if (run) begin
 						smpl_cnt_rst = 1;
 						trig_cnt_rst = 1;
 						nxt_state = RUN;
 					end else	
 						nxt_state = IDLE;
-			
-			RUN: 	if (wrt_smpl) begin
+					end
+
+						
+			RUN: 	begin
+					if (wrt_smpl) begin
 						we = 1;
-						//nxt_state = TRIG;
-						if (waddr < ENTRIES - 1)
+						if (waddr < ENTRIES - 1) 
 							waddr_inc = 1;
-						else
+						else 
 							waddr_rst = 1;
-						if(triggered) begin
-							trig_cnt_inc = 1;
-							nxt_state = TRIG;
-						end else begin
-							smpl_cnt_inc = 1;
-							nxt_state = SMPL;
-						end
-					end else 
+							
+					if(triggered) begin
+						trig_cnt_inc = 1;
+						nxt_state = TRIG;
+					end else begin
+						smpl_cnt_inc = 1;
+						nxt_state = SMPL;
+					end
+					end	else 
 						nxt_state = RUN;
+					end
 					
-			TRIG:	if (trig_cnt == trig_pos) begin
+			TRIG:	begin
+					if (trig_cnt == trig_pos) begin
 						we = 1;
 						set_capture_done = 1'b1;
 						clr_armed = 1'b1;
 						nxt_state = DONE;
 					end else
 						nxt_state = RUN;
+					end
+					
 					
 			SMPL:	begin
+						nxt_state = RUN;
 						if (smpl_cnt + trig_pos == ENTRIES) begin
 							set_armed = 1;
 						end 
-						nxt_state = RUN;
 					end
 		
-			DONE:	if (capture_done)
+			default:begin
+					if (capture_done)
 						nxt_state = DONE;
 					else
 						nxt_state = IDLE;
-		
+					end
 		endcase//endcase state
 	end
 	
